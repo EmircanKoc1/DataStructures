6395b81c19acbb43df3dc901e326bb15fa190a3c
Emircan <emircandv787@gmail.com>
2023-12-28 07:34:11 +0300

    c

---
+++ DLL2/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataStructures.SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        public static void Sort<T>(T[] array)
+            where T : IComparable
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        Sorting.Swap(array, j, j + 1);
+                    }
+
+                }
+            }
+        
+            
+
+        }
+
+       
+    }
+}
---
+++ DLL2/SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.SortingAlgorithms
+{
+    public class SelectionSort
+    {
+        public static void Sort<T>(T[] array)
+            where T : IComparable
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                var minValue = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(minValue) < 0)
+                    {
+                        minIndex = j;
+                        minValue = array[j];
+                    }
+
+                }
+                Sorting.Swap(array, i, minIndex);
+            }
+        }
+
+        public static void Sort<T>(T[] array, Shared.SortDirection sortDirection = Shared.SortDirection.Asceding)
+            where T : IComparable
+        {
+            var comparer = new Shared.CustomComparer<T>(sortDirection, Comparer<T>.Default);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array[j], array[i]) >= 0)
+                        continue;
+
+                    Sorting.Swap<T>(array, i, j);
+                }
+
+
+            }
+
+
+
+        }
+
+    }
+}
